97c1697c6d816d179a7f33564b84bf29d650ed15
divyamadlapally <divyamadlapally@gmail.com>
2023-04-16 21:11:16 +0530

    05-DynamicVariables.sh

--- 05-DynamicVariables.sh
+++ 05-DynamicVariables.sh
@@ -3,4 +3,4 @@ DATE="16-04-2023"
 echo "Good Morning, Todays date is $DATE"
 DATE=$(date +%F)
 echo "Todays date is $DATE"
-echo "Total no of opened sessions is $(who | wc -l)"
+echo -e "\e[32m Total no of opened sessions is $(who | wc -l) \e[0m"
