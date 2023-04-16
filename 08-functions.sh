d6353908d3e479053c15db5b860d422976c7e8e7
divyamadlapally <divyamadlapally@gmail.com>
2023-04-16 23:44:44 +0530

    08-functions.sh

--- 08-functions.sh
+++ 08-functions.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-sample() {
-    echo "I am a sample function"
-    echo "If you want to call me,  just type sample"
-    echo "sample function is completed"
-}
+# sample() {
+#     echo "I am a sample function"
+#     echo "If you want to call me,  just type sample"
+#     echo "sample function is completed"
+# }
 
-sample
+# sample
 
-stst() {
+stat() {
     echo "no of opened sessions : ${who |wc -l}"
     echo " Todays date is:${date +%F}"
     echo " Load average of the system in last 1 minute: ${uptime | awk -F : '{PRINT $NF}' | awk -F , '{PRINT $1}'} "
