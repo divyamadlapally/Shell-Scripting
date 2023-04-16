f4e37e1451b1af38e02df468868367b36ee9d4f9
divyamadlapally <divyamadlapally@gmail.com>
2023-04-16 19:54:14 +0530

    03-colours.sh

--- 03-colours.sh
+++ 03-colours.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo -e "\e[32m I am printing Green Colour \e[0m"
 echo -e "\e[31m I am printing Red Colour \e[0m" 
+# To print Background Colour
+echo -e "\e[41; 32m Printing colours \e[0m"
