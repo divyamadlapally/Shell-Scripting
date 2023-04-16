91f5b0b3428c55bd0d0e84e4a7f89c0d4b4c26b7
divyamadlapally <divyamadlapally@gmail.com>
2023-04-16 21:24:01 +0530

    06-SpecialVariables.sh

--- 06-SpecialVariables.sh
+++ 06-SpecialVariables.sh
@@ -3,5 +3,6 @@ echo "Name of the Script is $0"
 a=$1
 b=$2
 echo " value of a is $a"
+echo " value of b is $b"
 echo "variables used in this script are : $*"
 echo " No of variables used in this script are : $#"
